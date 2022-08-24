a21b699f51052c3fac29b65f597a88dad9dcade0
Devanand Yadav <devanandraj2002@gmail.com>
2022-08-24 10:33:33 +0530

    minimum subset sum diff

---
+++ 0-1 Knapsack/minimum_subset_sum_diff.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int minimumSubsetSumDiff(int arr[], int n) {
+    int count = 0;
+
+    return count;
+}
+
+int main()
+{
+    int arr[] = {1, 6, 11, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    cout<<"Minimum subset sum difference of a given set be: \t"<<minimumSubsetSumDiff(arr, n)<<"\n";
+}
