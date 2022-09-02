831f27ff4687b2c547075769a1a59c900ef64a34
Devanand Yadav <devanandraj2002@gmail.com>
2022-09-02 10:33:25 +0530

    mcm memoized version added

---
+++ Matrix_Chain_multiplication/matrix_chain_multiplication_memoized.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int static t[1001][1001];
+
+int solve(int arr[], int i, int j){
+    // Base condition 
+    if (i >= j)
+        return 0;
+
+    // memoized
+    if(t[i][j]!=-1)
+        return t[i][j];
+    
+
+    int ans = INT_MAX;
+    for(int k=i; k<=j-1; k++){
+        int tempname = solve(arr, i, k)+ solve(arr, k+1, j)+ arr[i-1]*arr[k]*arr[j];
+        ans = min(ans, tempname);
+    }
+
+    return t[i][j] = ans;
+}
+
+int main()
+{
+       int arr[] ={40,20,30,10,30};
+       int n = sizeof(arr)/sizeof(arr[0]);
+       memset(t, -1, sizeof(t));
+       
+       cout<<"Minimum cost of matrix chain multiplication:\t"<<solve(arr, 1, n-1)<<"\n";
+}
